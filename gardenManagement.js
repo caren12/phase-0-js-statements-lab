e1c7319b9ad715df809e61d29499d300473c2e19
Caren Chemjor <carenjeropchemjor@gmail.com>
2026-03-29 00:42:03 +0300

    completed assignment

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,40 @@
-const temperature = 90;
-const timeOfDay = "morning";
-let soilMoisture = 30;
+const temperature = 90
+const timeOfDay = "morning"
+let soilMoisture = 30
+
+// Temperature check
+
+function temperatureCheck() {
+
+    if  (temperature > 80) {
+
+console.log("Watering on");
+
+    } else {
+        console.log("Watering off");
+}
+}
+
+temperatureCheck()
+
+// Time of day check
+
+if (timeOfDay === "night") {
+
+    console.log("Lights on")}
+
+    else {
+        console.log("Lights off")
+}
+
+
+//Soil moisture increase
+
+while (soilMoisture < 40) {
+    soilMoisture +=5;
+    console.log(soilMoisture)
+
+}
+
+
+
